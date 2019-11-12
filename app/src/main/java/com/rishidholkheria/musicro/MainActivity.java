04af8e9ad8cb993327458e0a85119f88e1e5be81
Om <danom1608@gmail.com>
2019-11-12 22:55:27 +0530

    last

--- app/src/main/java/com/rishidholkheria/musicro/MainActivity.java
+++ app/src/main/java/com/rishidholkheria/musicro/MainActivity.java
@@ -1,229 +1,29 @@
 package com.rishidholkheria.musicro;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.Manifest;
-import android.app.ActivityManager;
-import android.app.PendingIntent;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Color;
-import android.graphics.drawable.GradientDrawable;
-import android.media.MediaPlayer;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.Parcelable;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toolbar;
+import android.os.Handler;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.io.IOException;
-import java.util.Random;
-import java.util.zip.Inflater;
-
-import static android.app.PendingIntent.getActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int SPLASH_TIME_OUT = 500;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.layout);
-
-        String [] color1 = {
-                "#FF006C",
-                "#00FF83",
-                "#B6FF00",
-                "#FF8700",
-                "#0CDF69",
-                "#0CB9DF"
-
-        };
-        String [] color2 = {
-
-                "#36FF33",
-                "#FFF633",
-                "#FF3333",
-                "#0CDFA6",
-                "#0C56DF",
-                "#DF0C7C"
-
-        };
-        Random r = new Random();
-        GradientDrawable gd = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT,
-
-                new int[]{Color.parseColor(color1[r.nextInt(6)]),Color.parseColor(color2[r.nextInt(6)])}
-
-        );
-
-        relativeLayout.setBackgroundDrawable(gd);
-
-
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-        return true;
-
-    }
-
-    private  static final String[] PERMISSIONS = {
-
-            Manifest.permission.READ_EXTERNAL_STORAGE
-
-    };
-
-    private static final int REQUEST_PERMISSIONS = 1001;
-
-    private  static final int PERMISSIONS_COUNT = 1;
-
-    private boolean PermissionDetails(){
-        for(int i = 0;i < PERMISSIONS_COUNT;i++){
-
-            if(checkSelfPermission(PERMISSIONS[i]) != PackageManager.PERMISSION_GRANTED){
-                return true;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splashIntent = new Intent(MainActivity.this,first_screen.class);
+                startActivity(splashIntent);
+                finish();
             }
-        }
-        return false;
+        },SPLASH_TIME_OUT);
+
     }
 
-  @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if(PermissionDetails()){
-            ((ActivityManager) (this.getSystemService(ACTIVITY_SERVICE))).clearApplicationUserData();
-            recreate();
-        }else {
-            onResume();
-        }
-    }
-
-    private boolean isMusicPlayerInit;
-
-    private List<String> musicFilesList;
-
-    private void addMusicFilesFrom(String dirPath){
-        final File musicDir = new File(dirPath);
-        if(!musicDir.exists()){
-            musicDir.mkdir();
-            return;
-        }
-        final File[] files = musicDir.listFiles();
-
-        for(File file : files){
-            final String path = file.getAbsolutePath();
-
-            if(path.endsWith(".mp3")){
-                musicFilesList.add(path);
-            }
-        }
-    }
-
-    private void fillMusicList(){
-        musicFilesList.clear();
-        addMusicFilesFrom(String.valueOf(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC)));
-        addMusicFilesFrom(String.valueOf(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)));
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && PermissionDetails()){
-            requestPermissions(PERMISSIONS, REQUEST_PERMISSIONS);
-            return;
-
-        }
-
-       if(!isMusicPlayerInit){
-           final ListView musiclist = (ListView) findViewById(R.id.musiclist);
-           final TextAdapter textAdapter = new TextAdapter();
-           musicFilesList = new ArrayList<>();
-           fillMusicList();
-           textAdapter.setData(musicFilesList);
-           musiclist.setAdapter(textAdapter);
-           musiclist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-               @Override
-               public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                   final String musicFilePath = musicFilesList.get(position);
-
-                   Intent i = new Intent(getApplicationContext(),play_music.class);
-                   i.putExtra("file_path",musicFilePath);
-                   startActivity(i);
-
-
-               }
-           });
-
-           isMusicPlayerInit = true;
-       }
-    }
-
-    class TextAdapter extends BaseAdapter{
-
-        private List<String> data = new ArrayList<>();
-
-        void setData(List<String> mData){
-            data.clear();
-            data.addAll(mData);
-
-            notifyDataSetChanged();
-        }
-
-        @Override
-        public int getCount() {
-            return data.size();
-        }
-
-        @Override
-        public String getItem(int position) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-
-            if(convertView == null){
-                convertView = LayoutInflater.from(parent.getContext()).
-                        inflate(R.layout.song, parent, false);
-                convertView.setTag(new ViewHolder((TextView)convertView.findViewById(R.id.song)));
-            }
-            ViewHolder holder = (ViewHolder) convertView.getTag();
-            final String song = data.get(position);
-
-            holder.info.setText(song.substring(song.lastIndexOf('/')+1));
-            return convertView;
-        }
-        class ViewHolder{
-            TextView info;
-
-            ViewHolder(TextView minfo){
-                info = minfo;
-            }
-        }
-    }
 
 }
--- app/src/main/java/com/rishidholkheria/musicro/play_music.java
+++ app/src/main/java/com/rishidholkheria/musicro/play_music.java
@@ -1,7 +1,13 @@
 package com.rishidholkheria.musicro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -9,36 +15,49 @@ import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RemoteViews;
 import android.widget.SeekBar;
 import android.widget.TextView;
+
 import java.util.Random;
 
 
 public class play_music extends AppCompatActivity {
+    final static String CHANNEL_ID = "current_playing_song";
+    final int NOTIFICATION_ID = 001;
     int songPosition = 00;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_music);
+        final TextView song_name = findViewById(R.id.song_name);
+        final TextView artist_name = findViewById(R.id.artist_name);
+        final ImageView album_art = findViewById(R.id.album_art);
         final MediaPlayer mp = new MediaPlayer();
+        final RemoteViews notificationView = new RemoteViews(getPackageName(), R.layout.notification_bar);
+
+
         final TextView songPositionTextView = findViewById(R.id.current_position);
         final TextView songDurationTextView = findViewById(R.id.song_duration);
         final Button pause = (Button) findViewById(R.id.pause);
-        TextView song_name = (TextView) findViewById(R.id.song_name);
-        TextView artist_name = (TextView) findViewById(R.id.artist_name);
-        ImageView album_art = (ImageView) findViewById(R.id.album_art);
+        final Button notification_pause = (Button) findViewById(R.id.notification_pause);
 
         final SeekBar seekBar = findViewById(R.id.seekbar);
         Intent i = getIntent();
         final String file_path = i.getStringExtra("file_path");
+
+
         LinearLayout LL = (LinearLayout) findViewById(R.id.layout_2);
 
         String[] color1 = {
+
                 "#FF006C",
                 "#00FF83",
                 "#B6FF00",
@@ -79,7 +98,7 @@ public class play_music extends AppCompatActivity {
             }
         }
 
-        final int songDuration = (mp.getDuration()/1000);
+        final int songDuration = (mp.getDuration() / 1000);
 
 
         seekBar.setMax(songDuration);
@@ -102,14 +121,15 @@ public class play_music extends AppCompatActivity {
                 mp.seekTo(songProgress);
             }
         });
-        songDurationTextView.setText(String.valueOf(songDuration/60) + ":" + String.valueOf(songDuration%60));
-        new Thread(){
-            public void run(){
-                songPosition=0;
-                while(songPosition < songDuration){
+        songDurationTextView.setText(String.valueOf(songDuration / 60) + ":" + String.valueOf(songDuration % 60));
+
+        new Thread() {
+            public void run() {
+                songPosition = 0;
+                while (songPosition < songDuration) {
                     try {
                         Thread.sleep(1000);
-                    }catch (InterruptedException e){
+                    } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                     songPosition++;
@@ -117,37 +137,54 @@ public class play_music extends AppCompatActivity {
                         @Override
                         public void run() {
                             seekBar.setProgress(songPosition);
-                            songPositionTextView.setText(String.valueOf(songPosition/60) + ":" + String.valueOf(songPosition%60));
+                            songPositionTextView.setText(String.valueOf(songPosition / 60) + ":" + String.valueOf(songPosition % 60));
                         }
                     });
 
                 }
             }
-        }.start();
+        };
 
-        byte art[];
 
+        byte art[] = null;
+
+        song_name.setText("Unknown Name");
+        album_art.setBackgroundColor(Color.GRAY);
+        Bitmap albumart;
         MediaMetadataRetriever mmr = new MediaMetadataRetriever();
         mmr.setDataSource(file_path);
         try {
             song_name.setText(mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE));
             art = mmr.getEmbeddedPicture();
-            Bitmap albumart = BitmapFactory.decodeByteArray(art, 0, art.length);
+            albumart = BitmapFactory.decodeByteArray(art, 0, art.length);
             album_art.setImageBitmap(albumart);
             artist_name.setText(mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST));
+            notificationView.setImageViewBitmap(R.id.notimg, albumart);
+            notificationView.setTextViewText(R.id.song_name, song_name.getText().toString());
+
 
         } catch (Exception e) {
             album_art.setBackgroundColor(Color.GRAY);
             song_name.setText("Unknown Name");
             artist_name.setText("Unknown Artist");
+            notificationView.setImageViewResource(R.id.notimg, R.drawable.cd);
+            notificationView.setTextViewText(R.id.song_name, "Unknown Song");
         }
 
         song_name.setMarqueeRepeatLimit(2);
 
 
+
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+
+            }
+        });
         pause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 if (mp.isPlaying()) {
                     mp.pause();
                     pause.setBackgroundResource(R.drawable.ic_play_circle_filled_black_24dp);
@@ -158,6 +195,45 @@ public class play_music extends AppCompatActivity {
             }
         });
 
+        /*notification_pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mp.isPlaying()) {
+                    mp.pause();
+                    notification_pause.setBackgroundResource(R.drawable.ic_play_circle_filled_black_24dp);
+                } else {
+                    mp.start();
+                    notification_pause.setBackgroundResource(R.drawable.ic_pause_circle_filled_black_24dp);
+                }
+            }
+        });*/
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            CharSequence name = "MusicRO Playback";
+            String description = "includes_current_playing_song";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+
+            notificationChannel.setDescription(description);
+
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID);
+        builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        builder.setContentTitle(song_name.getText().toString());
+        builder.setSmallIcon(R.drawable.notification_icon)
+                .setOnlyAlertOnce(true);
+        //builder.setContent(notificationView);
+        builder.addAction(R.id.notification_pause, "play/pause", null);
+        builder.setDefaults(Notification.FLAG_NO_CLEAR)
+                .setDefaults(Notification.BADGE_ICON_SMALL)
+                .setDefaults(Notification.DEFAULT_LIGHTS);
+        builder.setStyle(new androidx.media.app.NotificationCompat.MediaStyle());
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(NOTIFICATION_ID, builder.build());
     }
+
 }
